73f772103879a617645fe222d6c1d17e53b0f40a
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-10-27 14:43:59 +0430

    Adds morese_code file wiht morse codes in a hash

---
+++ morse_code.rb
@@ -0,0 +1,36 @@
+@mores_codes = {
+  A: '.-',
+  B: '-...',
+  C: '-.-.',
+  D: '-..',
+  E: '.',
+  F: '..-.',
+  G: '--.',
+  H: '....',
+  I: '..',
+  J: '.---',
+  K: '-.-',
+  L: '.-..',
+  M: '--',
+  N: '-.',
+  O: '---',
+  P: '.--.',
+  Q: '--.-',
+  R: '.-.',
+  S: '...',
+  T: '-',
+  U: '..-',
+  V: '...-',
+  W: '.--',
+  X: '-..-',
+  Y: '-.--',
+  Z: '--..'
+}
+
+def decode_char(str)
+  @mores_codes.each do |key, value|
+    return key if str == value
+  end
+end
+
+puts decode_char '.-'
